55ae0359d57ffdd3fb32ea946a8f606611eea572
Yassine MAKHLOUK <yassinemakhlouk0@gmail.com>
2024-01-15 17:39:57 +0100

    Get User(static)

--- src/main/java/ma/org/licence/spring_boot_demo1/api/model/User.java
+++ src/main/java/ma/org/licence/spring_boot_demo1/api/model/User.java
@@ -1,4 +1,66 @@
 package ma.org.licence.spring_boot_demo1.api.model;
 
 public class User {
+
+    //Attributes
+    private int id;
+    private String name;
+    private int age;
+    private String email;
+    private String pwd;
+
+
+    //Constructors
+
+    public User(int id, String name, int age, String email, String pwd) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.pwd = pwd;
+    }
+
+
+    //Getters & Setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
 }
--- src/main/java/ma/org/licence/spring_boot_demo1/services/UserService.java
+++ src/main/java/ma/org/licence/spring_boot_demo1/services/UserService.java
@@ -1,4 +1,38 @@
 package ma.org.licence.spring_boot_demo1.services;
 
+import ma.org.licence.spring_boot_demo1.api.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private List<User> userList;
+
+    public UserService(){
+        userList = new ArrayList<>();
+
+        User user1 = new User(1, "Yassine", 28, "yassine@email.com", "123456");
+        User user2 = new User(2, "Saad", 29, "saad@email.com", "123456");
+        User user3 = new User(3, "Abdou", 26, "abdou@email.com", "123456");
+        User user4 = new User(4, "Amine", 17, "amine@email.com", "123456");
+
+        userList.addAll(Arrays.asList(user1, user2, user3, user4));
+    }
+    public Optional<User> getUser(Integer id) {
+        Optional optional = Optional.empty();
+        for (User user:userList ){
+            if (id == user.getId()) {
+                optional = Optional.of(user);
+
+                return optional;
+            }
+
+        }
+        return optional;
+    }
 }
